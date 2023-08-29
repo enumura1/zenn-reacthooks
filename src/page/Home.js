6274c63fe875242817d830b6705e10ea5a986b93
utoNakamu <akirayuto0718@gmail.com>
2023-08-30 00:56:01 +0900

    add:materialui toggle switch

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,8 +1,19 @@
-import '../css/Home.css';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+
+// 外部ファイルのインポート
+import '../css/Home.css';
 
 function Home() {
   const navigate = useNavigate();
+
+  const [switchChecked, setSwitchChecked] = useState(true);
+
+  const toggleSwitch = () => {
+    setSwitchChecked(!switchChecked);
+  };
 
   const clickedToHomeBtn = () => {
     navigate('/');
@@ -29,15 +40,22 @@ function Home() {
   };
 
   return (
-    <div className='mainContainer'>
-      <h1>Homeページ</h1>
-      <button onClick={clickedToHomeBtn}>Homeボタン</button>
-      <button onClick={clickedToPage1Btn}>useStateページ</button>
-      <button onClick={clickedToPage2Btn}>useRefページ</button>
-      <button onClick={clickedToPage3Btn}>useContextページ</button>
-      <button onClick={clickedToPage4Btn}>カスタムフックページ</button>
-      <button onClick={clickedToPage5Btn}>useNavigateページ</button>
-    </div>
+    <>
+      <div className='themeSwitch'>
+        <FormControlLabel
+          control={<Switch checked={switchChecked} onChange={toggleSwitch} />}
+          label={switchChecked ? 'Checked Label' : 'Unchecked Label'} />
+      </div>
+      <div className='mainContainer'>
+        <h1>Homeページ</h1>
+        <button onClick={clickedToHomeBtn}>Homeボタン</button>
+        <button onClick={clickedToPage1Btn}>useStateページ</button>
+        <button onClick={clickedToPage2Btn}>useRefページ</button>
+        <button onClick={clickedToPage3Btn}>useContextページ</button>
+        <button onClick={clickedToPage4Btn}>カスタムフックページ</button>
+        <button onClick={clickedToPage5Btn}>useNavigateページ</button>
+      </div>
+    </>
   );
 };
 
