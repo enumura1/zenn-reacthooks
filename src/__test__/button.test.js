d2ead9cdb008fbf0ba7a87311b50953a2d0a2b54
enumura1 <akirayuto0718@gmail.com>
2023-09-12 23:41:42 +0900

    text:isbbtn component and text

--- src/__test__/button.test.js
+++ src/__test__/button.test.js
@@ -2,9 +2,20 @@ import { render, screen } from "@testing-library/react";
 import App from "../App";
 
 describe("Test Button Component", () => {
+    //Number of button components
     test("render form with 5 button", async () => {
         render(<App />);
         const buttonList = await screen.findAllByRole("button");
         expect(buttonList).toHaveLength(5);
     });
+    // button text
+    test('render buttons text', () => {
+        render(<App />);
+        const textsToCheck = ['useState Page', 'useEffect Page', 'useContext Page', 'useRef Page', 'Custom Hook Page'];
+        textsToCheck.forEach((text) => {
+            const linkElement = screen.getByText(new RegExp(text, 'i'));
+            expect(linkElement).toBeInTheDocument();
+        });
+    });
+
 });
